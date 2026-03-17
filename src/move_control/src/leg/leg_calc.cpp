a1758b36557094eaa26b7a1d17d0600eb0683cf6
lyz1202dt <cloud1202@qq.com>
2026-03-17 23:51:55 +0800

    修正动力学数值计算错误

--- src/move_control/src/leg/leg_calc.cpp
+++ src/move_control/src/leg/leg_calc.cpp
@@ -132,7 +132,7 @@ Eigen::Vector3d
     _temp2_joint3_array(1) = joint_omega[1];
     _temp2_joint3_array(2)  = joint_omega[2];
     dynamin_solver.JntToGravity(_temp_joint3_array, G);
-    dynamin_solver.JntToCoriolis(_temp_joint3_array, _temp_joint3_array, C);
+    dynamin_solver.JntToCoriolis(_temp_joint3_array, _temp2_joint3_array, C);
     dynamin_solver.JntToMass(_temp_joint3_array, M);
 
     // 6. 转换 KDL 输出到 Eigen，方便矩阵运算
